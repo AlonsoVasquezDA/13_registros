8b6fd2f3b0e9d8665f3dc938086c45e38b9f847b
AlonsoVasquezDA <alonsovasquez1409@gmail.com>
2025-06-18 20:55:37 -0500

    ejemplo1:Hallar estadisticas de e informacion de trabajadores

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -17,12 +17,22 @@ int main(){
         cin >>EMPLEADOS[i].num;
         cout<<"Escriba el nombre del empleado: ";
         cin >>EMPLEADOS[i].nom;
+        cout<<"Ingrese el dinero de venta: "<<endl;
         for(int j=0 ; j<12 ; j++){
-            cout<<"Ingrese el dinero de venta: "<<endl;
             cin >>EMPLEADOS[i].ven[j];
         }
             cout<<"Ingrese su salario: ";
             cin >>EMPLEADOS[i].salario;
+    }
+    for (int i=0; i<n ; i++){
+        cout<<"Numero del trabajador: " <<EMPLEADOS[i].num<<endl;
+        cout<<"Nombre del trabajador: "<<EMPLEADOS[i].nom<<endl;
+        cout<<"Cantidad de ventas: ";
+        for (int j=0; j<12 ; j++){
+            cout<<EMPLEADOS[i].ven[j]<< " ";
+        }
+    cout<<"\n";;
+    cout<<"Su cantidad de salario: "<<EMPLEADOS[i].salario<<endl<<endl;
     }
     return 0;
 }
