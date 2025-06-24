eac6a63561d6ff61d7356fb90c170d07e882467f
AlonsoVasquezDA <alonsovasquez1409@gmail.com>
2025-06-24 11:44:33 -0500

    guardado

--- registros3.cpp
+++ registros3.cpp
@@ -7,17 +7,39 @@ using namespace std;
 };
 int main (){
     PER PERSONAS[100];
-    int n;
+    int n,mes;
     cout<<"Ingrese la cantidad de personas: ";
     cin>>n;
+    cout<<endl;
     for (int i=0 ; i<n ; i++ ){
         cout<<"Escriba su nombre: ";
         cin>>PERSONAS[i].nom;
-        cout<<"Escrina su dia de nacimiento ( dd / mm / aaaa )";
-        cin>>PERSONAS[i].fecha[0]>>PERSONAS[i].fecha[0]>>PERSONAS[i].fecha[0];
+        cout<<"Escrina su dia de nacimiento ( dd / mm / aaaa ) ";
+        cin>>PERSONAS[i].fecha[0]>>PERSONAS[i].fecha[1]>>PERSONAS[i].fecha[2];
         cout<<endl;
     }
-    for ( int i=0 ; i<n ; i++ ){
-        cout<<
+    for (int i=0 ; i<n ; i++ ){
+        cout<<PERSONAS[i].nom<<"   ";
+        cout<<PERSONAS[i].fecha[0];
+        cout<<PERSONAS[i].fecha[1];
+        cout<<PERSONAS[i].fecha[2];
+        cout<<endl;
+        cout<<"-------------------"<<endl;
     }
-}
+    cout<<"Ingrese un mes ( mm ): ";
+    cin>>mes;
+    while ( mes != 0 ){
+        for ( int i=0 ; i<n ; i++){
+            if ( PERSONAS[i].fecha[1] == mes ){
+                cout<<PERSONAS[i].nom<<"   ";
+                cout<<PERSONAS[i].fecha[0]<<" / ";
+                cout<<PERSONAS[i].fecha[1]<<" / ";
+                cout<<PERSONAS[i].fecha[2];
+                cout<<endl;
+            }
+        }
+    cout<<"Ingrese un mes ( mm ): ";
+    cin>>mes;
+    }
+    return 0;
+} 
